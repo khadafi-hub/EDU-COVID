16128cfc514ffa05270dab2726fc84200b042530
khadafi-hub <56795052+khadafi-hub@users.noreply.github.com>
2021-03-27 02:50:41 +0700

    Update contact_mail.php

--- contact_mail.php
+++ contact_mail.php
@@ -1,5 +1,5 @@
 <?php
-$toEmail = "khadafikhadafi0@gmail.com.com";
+$toEmail = "khadafikhadafi0@gmail.com";
 $mailHeaders = "From: " . $_POST["name"] . "<". $_POST["email"] .">\r\n";
 if(mail($toEmail, $_POST["subject"], $_POST["mesage"], $mailHeaders)) {
 print "<p class='success'>Contact Mail Sent.</p>";
