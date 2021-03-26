bf3ec11a4f1d4b948d3b2c2901c5eb6a97ef6553
khadafi-hub <56795052+khadafi-hub@users.noreply.github.com>
2021-03-27 03:58:42 +0700

    Update contact_mail.php

--- contact_mail.php
+++ contact_mail.php
@@ -1,9 +1,32 @@
 <?php
-$toEmail = "khadafikhadafi0@gmail.com";
-$mailHeaders = "From: " . $_POST["#userName"] . "<". $_POST["#userEmail"] .">\r\n";
-if(mail($toEmail, $_POST["#subject"], $_POST["#content"], $mailHeaders)) {
-print "<p class='success'>Contact Mail Sent.</p>";
-} else {
-print "<p class='Error'>Problem in Sending Mail.</p>";
+
+$userName = $_POST["userName"];
+$userEmail = $_POST["userEmail"];
+$subject = $_POST["subject"];
+$content = $_POST["content"];
+
+
+$EmailTo = "khadafikhadafi0@gmail.com";
+
+// prepare email body text
+$Body .= "Name: ";
+$Body .= $userName;
+$Body .= "\n";
+ 
+$Body .= "Email: ";
+$Body .= $userEmail;
+$Body .= "\n";
+ 
+$Body .= "Message: ";
+$Body .= $content;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $subject, $Body, "From:".$userEmail);
+ 
+// redirect to success page
+if ($success){
+   print "<p class='success'>Contact Mail Sent.</p>";
+}else{
+    print "<p class='Error'>Problem in Sending Mail.</p>";
 }
-?>
